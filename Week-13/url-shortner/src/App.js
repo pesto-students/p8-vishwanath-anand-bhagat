40caebbd4c3c3adfb3b2d55ee597bbe345b4e5b3
anand-bhagat <anbhagat1997@gmail.com>
2023-03-11 18:42:47 +0530

    added new page for contact us and have implemented useContext

--- Week-13/url-shortner/src/App.js
+++ Week-13/url-shortner/src/App.js
@@ -1,15 +1,40 @@
-import Header from "./components/Header/Header";
 import './App.css';
-import Footer from "./components/Footer/Footer";
-import MainContent from "./components/MainContent/MainContent";
+import {
+	createBrowserRouter,
+	RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home/Home";
+import ContactUs from './pages/ContactUs/ContactUs';
+import { createContext, useState } from 'react';
 
+const router = createBrowserRouter([
+	{
+	  path: "/",
+	  element: (<Home />),
+	},
+	{
+	  path: "/contact_us",
+	  element: (<ContactUs />),
+	},
+]);
+
+export const ShortenLinksContext = createContext({
+	shortenLinks: [],
+	setShortenLinks: () => {}
+});
 
 const App = () => {
+	const [shortenLinks, setShortenLinks] = useState([{    
+		"id": 0,    
+		"link": "https://chat.openai.com/chat",    
+		"shorten_link": "https://shrtco.de/MIoV82"  
+	}]);
+
 	return (
 		<div className="App">
-			<Header />
-			<MainContent />
-			<Footer />
+			<ShortenLinksContext.Provider value={{shortenLinks, setShortenLinks}}>
+				<RouterProvider router={router} />
+			</ShortenLinksContext.Provider>
 		</div>
 	);
 }
--- Week-13/url-shortner/src/components/Header/Header.js
+++ Week-13/url-shortner/src/components/Header/Header.js
@@ -1,16 +1,22 @@
 import React from 'react'
+import { Link, useLocation } from 'react-router-dom';
 import styles from './Header.module.css';
 
 const Header = () => {
-  return (
-    <div className={styles.main}>
-        <div className={styles.container}>
-            <h2>
-                Shortly
-            </h2>
-        </div>
-    </div>
-  )
+	const location = useLocation();
+	return (
+		<div className={styles.main}>
+			<div className={styles.container}>
+				<h2>
+					Shortly
+				</h2>
+				<nav className={styles.navigation}>
+					<li className={location.pathname === '/' ? styles.active : ''}><Link to="/">Home</Link></li>
+					<li className={location.pathname === '/contact_us' ? styles.active : ''}><Link to="/contact_us">Contact Us</Link></li>
+				</nav>
+			</div>
+		</div>
+	)
 }
 
 export default Header;
